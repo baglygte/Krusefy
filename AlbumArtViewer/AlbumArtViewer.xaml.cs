8c56786b57e3c4b015c77c4f7ab691b11a61d1d5
Patrick Kruse <baglygte@gmail.com>
2025-03-16 07:32:41 +0100

    Moved things into folders

---
+++ AlbumArtViewer/AlbumArtViewer.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace Krusefy
+{
+    public partial class AlbumArtViewer : UserControl
+    {
+        public AlbumArtViewer()
+        {
+            InitializeComponent();
+        }
+    }
+}
